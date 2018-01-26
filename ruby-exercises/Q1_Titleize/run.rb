df441f802185156aec75a820615dab7359df9145
Tommy L <tommykwlee@gmail.com>
2018-01-25 16:21:29 -0800

    Condense files for Q1 and superceded include_to.rb and extend_to.rb

--- ruby-exercises/Q1_Titleize/run.rb
+++ ruby-exercises/Q1_Titleize/run.rb
@@ -1,5 +1,12 @@
-load './include_to.rb'
-load './extend_to.rb'
+require './helper_method.rb'
+
+class ExtendTo
+  extend HelperMethod
+end
+
+class IncludeTo
+  include HelperMethod
+end
 
 puts "\nThe following title is made from including the HelperMethod module to a class"
 puts IncludeTo.new.titleize('harry potter and the philosopher\'s stone')
@@ -8,4 +15,3 @@ puts IncludeTo.new.titleize('harry potter and the philosopher\'s stone')
 
 puts "\nThe following title is made from extending the HelperMethod module to a class"
 puts ExtendTo.titleize('harry potter and the deathly hallows')
-puts

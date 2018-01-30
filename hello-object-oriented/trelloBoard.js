5686aac8d772d5217e5ca27984b339644fc4bedc
Tommy L <tommykwlee@gmail.com>
2018-01-29 18:31:20 -0800

    Include checks to prevent duplicate tasks or lists

--- hello-object-oriented/trelloBoard.js
+++ hello-object-oriented/trelloBoard.js
@@ -24,12 +24,24 @@ class List {
     this.listOfTasks = [];
   }
 
-  addTask(taskObject) {
-    this.listOfTasks.push(taskObject);
+
+  /* The entire 'for' block checks if the task being added already exists in the
+  list. If there would be a repeating task name, the assignees would join
+  together under the same task instead */
+  addTask(newTaskObject) {
+    for (let task of this.listOfTasks) {
+      if (task.taskName === newTaskObject.taskName) {
+        task.assignee = [task.assignee, newTaskObject.assignee].filter(x => x !==null).join(', ');
+        if (task.assignee === "") task.assignee = null;
+        return this;
+      }
+    }
+
+    // These two lists of code would be good enough for most cases
+    this.listOfTasks.push(newTaskObject);
     return this;
   }
 
-  // Using an array for this. In
   removeTask(taskName) {
     let outArray = this.listOfTasks.filter(taskObject => taskObject.taskName === taskName);
     if (outArray.length === 0) return null;
@@ -54,8 +66,23 @@ class Board {
     this.listOfLists = [];
   }
 
-  addList(listObject) {
-    this.listOfLists.push(listObject);
+  /* The entire outside 'for' block checks if the list being added already exists in the
+  board. If there would be a repeating list name, the tasks would just combine
+  under the same list instead. The Inside 'for' block leads the addTask method of
+  of the Task class, which then makes sure we avoid repeated tasks.*/
+
+  addList(newListObject) {
+    for (let list of this.listOfLists) {
+      if (list.listName === newListObject.listName) {
+        for (let newListTask of newListObject.listOfTasks) {
+          list.addTask(newListTask);
+        }
+        return this;
+      }
+    }
+
+    // These two lists of code would be good enough for most cases
+    this.listOfLists.push(newListObject);
     return this;
   }
 
@@ -69,6 +96,7 @@ class Board {
   moveTaskTo(taskName, firstListName, secondListName) {
     let firstListObject = null;
     let secondListObject = null;
+
     for (let listObject of this.listOfLists) {
       if (listObject.listName === firstListName) {
         firstListObject = listObject;
@@ -95,29 +123,37 @@ class Board {
 
 const toDoList = new List('To Do')
   .addTask(new Task('Laundry', 'You'))
+  .addTask(new Task('Laundry', 'Me'))
   .addTask(new Task('Buy Apples'))
   .addTask(new Task('Pay Phone Bill', 'Me'));
 
-const doingList = new List('Doing')
-  .addTask(new Task('Study JavaScript', 'Jill'))
-  .addTask(new Task('Study HTML', 'Jill'))
-  .addTask(new Task('Study Ruby', 'Me'));
+const doingList = new List('Doing').addTask(new Task('Study JavaScript', 'Jill')).addTask(new Task('Study HTML', 'Jill')).addTask(new Task('Study Ruby', 'Me'));
 
 const doneList = new List('Done')
   .addTask(new Task('Laundry'))
-  .addTask(new Task('Ruby Exercises Homework'));
+  .addTask(new Task('Ruby Exercises Homework', 'Everyone'));
 
 const myBoard = new Board('Big Shiny Board')
   .addList(toDoList)
   .addList(doingList)
   .addList(doneList);
 
+
 console.log(myBoard.render());
 
+toDoList.listOfTasks[0].assignee = "Me"
 myBoard.moveTaskTo('Laundry', 'To Do', 'Doing')
   .moveTaskTo('Buy Apples', 'To Do', 'Doing')
   .moveTaskTo('Random', 'To Do', 'Doing')
   .moveTaskTo('Buy Apples', 'Random', 'Doing')
   .moveTaskTo('Buy Apples', 'To Do', 'Random');
+
+const doneList2 = new List('Done')
+  .addTask(new Task('Rule the World'))
+  .addTask(new Task('Move like Jagger'))
+  .addTask(new Task('Ruby Exercises Homework', 'Not Me'));
+
+myBoard.addList(doneList2)
+
 
 console.log(myBoard.render());

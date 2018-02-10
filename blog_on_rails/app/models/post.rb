2f38a5c47ab1c2ef40951d20793205654adadf2b
Tommy L <tommykwlee@gmail.com>
2018-02-09 18:30:55 -0800

    Add bootstrap, posts controller and resouece (posts), and post model

---
+++ blog_on_rails/app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+
+validates :title, presence: true, uniqueness: true
+validates :body, presence: true, length: { minimum: 50 }
+
+
+
+end
--- blog_on_rails/config/puma.rb
+++ blog_on_rails/config/puma.rb
@@ -9,8 +9,9 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
-
+port_num = 3000
+port        ENV.fetch("PORT") { port_num }
+puts "Server up at  http://localhost:#{ port_num }".colorize :red
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
